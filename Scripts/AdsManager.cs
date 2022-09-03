e626530e78b831a90e3a0c822cc0af150eef36d6
harshu944 <66071505+harshu944@users.noreply.github.com>
2022-09-03 23:05:59 +0530

    Add files via upload

---
+++ Scripts/AdsManager.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+public class AdsManager : MonoBehaviour
+{
+    private BannerView bannerView;
+    private InterstitialAd interstitial;
+    private RewardedAd rewardedAd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MobileAds.Initialize(initStatus => { });
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void RequestBanner()
+    {
+#if UNITY_ANDROID
+            string adUnitId = "ca-app-pub-3940256099942544/6300978111";
+#elif UNITY_IPHONE
+            string adUnitId = "ca-app-pub-3940256099942544/2934735716";
+#else
+        string adUnitId = "unexpected_platform";
+#endif
+
+        // Create a 320x50 banner at the top of the screen.
+        bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Top);
+        AdRequest request = new AdRequest.Builder().Build();
+
+        // Load the banner with the request.
+        this.bannerView.LoadAd(request);
+    }
+    public void RequestInterstitial()
+    {
+#if UNITY_ANDROID
+        string adUnitId = "ca-app-pub-7089128764525489/4043033970";
+#elif UNITY_IPHONE
+        string adUnitId = "ca-app-pub-3940256099942544/4411468910";
+#else
+        string adUnitId = "unexpected_platform";
+#endif
+
+        // Initialize an InterstitialAd.
+        interstitial = new InterstitialAd(adUnitId);
+        AdRequest request = new AdRequest.Builder().Build();
+        // Load the interstitial with the request.
+        interstitial.LoadAd(request);
+        interstitial.OnAdLoaded += Interstitial_OnAdLoaded;
+    }
+
+    private void Interstitial_OnAdLoaded(object sender, System.EventArgs e)
+    {
+        interstitial.Show();
+    }
+
+    public void CreateAndLoadRewardedAd()
+    {
+#if UNITY_ANDROID
+            string adUnitId = "ca-app-pub-7089128764525489/9103788963";
+#elif UNITY_IPHONE
+            string adUnitId = "ca-app-pub-3940256099942544/1712485313";
+#else
+        string adUnitId = "unexpected_platform";
+#endif
+
+        rewardedAd = new RewardedAd(adUnitId);
+
+        rewardedAd.OnAdLoaded += RewardedAd_OnAdLoaded;
+        rewardedAd.OnUserEarnedReward += RewardedAd_OnUserEarnedReward;
+        rewardedAd.OnAdClosed += RewardedAd_OnAdClosed;
+
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+        // Load the rewarded ad with the request.
+        rewardedAd.LoadAd(request);
+    }
+
+    private void RewardedAd_OnAdClosed(object sender, System.EventArgs e)
+    {
+        //ad has been closed by the user
+    }
+
+    private void RewardedAd_OnUserEarnedReward(object sender, Reward e)
+    {
+        //reward your user
+    }
+
+    private void RewardedAd_OnAdLoaded(object sender, System.EventArgs e)
+    {
+        rewardedAd.Show();
+
+    }
+}
---
+++ Scripts/UiManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UiManager : MonoBehaviour
+{
+    public static UiManager instance;
+    public GameObject infiniteballpath;
+    public GameObject gameOverPanel;
+    public GameObject tapText;
+    public GameObject leaderBoardButton;
+   
+    public Text score;
+    public Text highscore1;
+    public Text highscore2;
+
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        highscore1.text = PlayerPrefs.GetInt("highScore").ToString();
+
+    }
+
+    public void GameStart()
+    {
+       
+
+        tapText.SetActive(false);
+        leaderBoardButton.SetActive(false);
+        infiniteballpath.GetComponent<Animator>().Play("PanelUp");
+    }
+    public void GameOver()
+    {
+        score.text = PlayerPrefs.GetInt("score").ToString();
+        highscore2.text = PlayerPrefs.GetInt("highScore").ToString();
+        gameOverPanel.SetActive(true);
+    }
+    public void Reset()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void Reset2()
+    {
+        SceneManager.LoadScene(1);
+    }
+   
+    public void doquit()
+    {
+        Debug.Log("has quit game");
+        Application.Quit();
+    }
+   
+  
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   
+}
